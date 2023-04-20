751e3ec6286c57e4bbb1afaa5f9cd399abde383e
NguyenThanhLong <nguyenthanhlong190502@gmail.com>
2023-04-20 23:13:46 +0700

    CRUD teacher and fix file teacher.schema.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import userRoute from './user/user.route.js'
 import studentRoute from './student/student.route.js';
 import statisticsRoute from './studentStatictics/statistics.route.js';
+import teacherRoute from "./teacher/teacher.route.js";
 const router = express.Router();
 
 router.use("/user", userRoute);
 router.use("/statistics", statisticsRoute);
 router.use("/students", studentRoute);
+router.use("/teacher", teacherRoute);
 
 
 export default router
