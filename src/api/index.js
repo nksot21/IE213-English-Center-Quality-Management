b2d07a84ed7ec90441405d0bb6cf8d6ed5899ed5
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-23 08:36:41 +0700

    complete create and update class report

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,14 @@ import studentRoute from './student/student.route.js';
 import statisticsRoute from './studentStatictics/statistics.route.js';
 import reportReportRoute from './studentReport/report.route.js'
 import teacherRoute from "./teacher/teacher.route.js";
+import classReportRoute from './classReport/classReport.route.js'
 const router = express.Router();
 
 router.use("/user", userRoute);
 router.use("/statistics", statisticsRoute);
 router.use("/students", studentRoute);
 router.use("/student-report", reportReportRoute)
+router.use("/class-report", classReportRoute);
 router.use("/teacher", teacherRoute);
 
 export default router
