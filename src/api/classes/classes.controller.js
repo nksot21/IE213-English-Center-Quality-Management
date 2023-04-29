f6b1d7aab6a3f86c87a4f64def1a9913a939e8f4
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-29 08:31:14 +0700

    add new api

--- src/api/classes/classes.controller.js
+++ src/api/classes/classes.controller.js
@@ -1,115 +1,141 @@
 import ClassSchema from "../model/class.schema.js";
 
 import Response from "../helpers/response.js";
+import StudentSchema from "../model/student.schema.js";
 
 export default class ClassesController {
-    //-------getAllClasses--------
-    static async getAllClasses(req, res, next) {
-        try {
-            const ClassType = req.query.classType;
-            const ScoreIncome = req.query.scoreIncome;
-            const ScoreDesire = req.query.scoreDesire;
-            if (!ClassType && !ScoreIncome && !ScoreDesire){
-                const classes = await ClassSchema.find();
-                if(!classes) {
-                    throw "error";
-                }
-    
-                return res.status(200).json(Response.successResponse(classes));
-            }
-            else{
-                const classes = await ClassSchema.find({Type: ClassType, 
-                    ScoreRequired: {$lte: ScoreIncome},
-                    ScoreTarget: {$gte: ScoreDesire}});
-                if(!classes) {
-                    throw "error";
-                }
-                return res.status(200).json(Response.successResponse(classes));
-            }
+  //-------getAllClasses--------
+  static async getAllClasses(req, res, next) {
+    try {
+      const ClassType = req.query.classType;
+      const ScoreIncome = req.query.scoreIncome;
+      const ScoreDesire = req.query.scoreDesire;
+      let classes;
+      if (!ClassType && !ScoreIncome && !ScoreDesire) {
+        classes = await ClassSchema.find();
+        if (!classes) {
+          throw "error";
         }
-        catch (error) {
-            return res.json(Response.handlingErrorResponse(error));
-        }
-    }
+      } else {
+        classes = await ClassSchema.find({
+          Type: ClassType,
+          ScoreRequired: { $lte: ScoreIncome },
+          ScoreTarget: { $gte: ScoreDesire },
+        });
 
-    //---------getClassesById---------
-    static async getClassesById(req, res, next) {
-        try {
-            const classes = await ClassSchema.findById(req.params.id);
-            if(!classes) {
-                throw "error";
-            }
-            return res.status(200).json(Response.successResponse(classes));
-        }
-        catch (error) {
-            return res.json(Response.handlingErrorResponse(error));
-        }
-    }
-
-
-    //-----------createClasses------------
-
-    static async createClasses(req, res) {
-        try {
-          console.debug("Creating Class...");
-          const { Type, ScoreTarget } = req.body;
-      
-          let ClassID;
-          if (Type.startsWith("TOE") && !isNaN(Type.substr(3)) && !isNaN(ScoreTarget)) {
-            ClassID = `TOE${ScoreTarget}${`.`}${Math.floor(100 + Math.random() * 900)}`;
-          } else if (Type.startsWith("IET") && !isNaN(Type.substr(3)) && !isNaN(ScoreTarget)) {
-            ClassID = `IET${ScoreTarget}${`.`}${Math.floor(100 + Math.random() * 900)}`;
-          } else {
-            return res.status(400).json(Response.errorResponse("Invalid Type or ScoreTarget"));
-          }
-      
-          const classExists = await ClassSchema.findOne({ ClassID });
-          if (classExists) {
-            return res.status(400).json(Response.errorResponse("ClassID already exists"));
-          }
-      
-          const newClass = new ClassSchema({ ...req.body, ClassID });
-          const savedClass = await newClass.save();
-          return res.status(201).json(Response.successResponse(savedClass));
-        } 
-        catch (error) {
-          return res.json(Response.handlingErrorResponse(error));
+        if (!classes) {
+          throw "error";
         }
       }
       
+      for (let i = 0; i < classes.length; i++) {
+        const classId = classes[i]._id;
+        console.log("classiD", classId);
+        const students = await StudentSchema.find({
+         ClassID:classId,
+        });
+       
+        console.log("students", students.length);
+        
+        //bug get number student in a class
+        classes[i].totalStudent = students.length;
+       console.log("students", classes[i]);
 
-    //-------------updateClass-------------
-    static async updateClasses(req, res) {
-        try {
-            console.debug("Updating...");
-            const { id } = req.params;
-            const updatedClass = await ClassSchema.findByIdAndUpdate(
-                id,
-                req.body,
-                { new: true }
-            );
-            if (!updatedClass) {
-                return res.status(404).json(Response.errorResponse("Class not found"));
-            }
-            return res.json(Response.successResponse(updatedClass));
-        } 
-        catch (error) {
-            return res.json(Response.handlingErrorResponse(error));
-        }
-    }
-    
-    //-------------deleteClass-------------
-    static async deleteClasses(req, res) {
-        try {
-          const ClassId = req.params.id; 
-          const deletedClass = await ClassSchema.findByIdAndDelete(ClassId); 
-          if (!deletedClass) { 
-            return res.status(404).json(Response.errorResponse("Class not found"));
-          }
-          return res.status(200).json(Response.successResponse("Class deleted successfully"));
-        } 
-        catch (error) {
-          return res.json(Response.handlingErrorResponse(error));
-        }
       }
+      return res.status(200).json(Response.successResponse(classes));
+    } catch (error) {
+      return res.json(Response.handlingErrorResponse(error));
+    }
+  }
+
+  //---------getClassesById---------
+  static async getClassesById(req, res, next) {
+    try {
+      const classes = await ClassSchema.findById(req.params.id);
+      if (!classes) {
+        throw "error";
+      }
+      return res.status(200).json(Response.successResponse(classes));
+    } catch (error) {
+      return res.json(Response.handlingErrorResponse(error));
+    }
+  }
+
+  //-----------createClasses------------
+
+  static async createClasses(req, res) {
+    try {
+      console.debug("Creating Class...");
+      const { Type, ScoreTarget } = req.body;
+
+      let ClassID;
+      if (
+        Type.startsWith("TOE") &&
+        !isNaN(Type.substr(3)) &&
+        !isNaN(ScoreTarget)
+      ) {
+        ClassID = `TOE${ScoreTarget}${`.`}${Math.floor(
+          100 + Math.random() * 900
+        )}`;
+      } else if (
+        Type.startsWith("IET") &&
+        !isNaN(Type.substr(3)) &&
+        !isNaN(ScoreTarget)
+      ) {
+        ClassID = `IET${ScoreTarget}${`.`}${Math.floor(
+          100 + Math.random() * 900
+        )}`;
+      } else {
+        return res
+          .status(400)
+          .json(Response.errorResponse("Invalid Type or ScoreTarget"));
+      }
+
+      const classExists = await ClassSchema.findOne({ ClassID });
+      if (classExists) {
+        return res
+          .status(400)
+          .json(Response.errorResponse("ClassID already exists"));
+      }
+
+      const newClass = new ClassSchema({ ...req.body, ClassID });
+      const savedClass = await newClass.save();
+      return res.status(201).json(Response.successResponse(savedClass));
+    } catch (error) {
+      return res.json(Response.handlingErrorResponse(error));
+    }
+  }
+
+  //-------------updateClass-------------
+  static async updateClasses(req, res) {
+    try {
+      console.debug("Updating...");
+      const { id } = req.params;
+      const updatedClass = await ClassSchema.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      if (!updatedClass) {
+        return res.status(404).json(Response.errorResponse("Class not found"));
+      }
+      return res.json(Response.successResponse(updatedClass));
+    } catch (error) {
+      return res.json(Response.handlingErrorResponse(error));
+    }
+  }
+
+  //-------------deleteClass-------------
+  static async deleteClasses(req, res) {
+    try {
+      const ClassId = req.params.id;
+      const deletedClass = await ClassSchema.findByIdAndDelete(ClassId);
+      if (!deletedClass) {
+        return res.status(404).json(Response.errorResponse("Class not found"));
+      }
+      return res
+        .status(200)
+        .json(Response.successResponse("Class deleted successfully"));
+    } catch (error) {
+      return res.json(Response.handlingErrorResponse(error));
+    }
+  }
 }
--- src/api/studentReport/report.controller.js
+++ src/api/studentReport/report.controller.js
@@ -158,206 +158,250 @@ async function getStudentReports({
 
 
 // Create or Update Report
-export default class studentReportController{
-    static async createStudentReport({date = null, attendance=null, homeworkScore = -1, testScore = -1, studentId=null, testScoreRequired = 0, homeworkScoreRequired = 0} = {}){
-        try{
-            if(!date || !studentId){
-                throw "Date and StudentId are needed to create report!"
-            }
-            // Find Report
-            const reportData = {
-                StudentID: studentId,
-                Date: new Date(date)
-            }
+export default class studentReportController {
+  static async createStudentReport({
+    date = null,
+    attendance = null,
+    homeworkScore = -1,
+    testScore = -1,
+    studentId = null,
+    testScoreRequired = 0,
+    homeworkScoreRequired = 0,
+  } = {}) {
+    try {
+      if (!date || !studentId) {
+        throw "Date and StudentId are needed to create report!";
+      }
+      // Find Report
+      const reportData = {
+        StudentID: studentId,
+        Date: new Date(date),
+      };
 
-            const reportDb = await StudentReportSchema.findOne(reportData)
-            if(!reportDb){
-                let student = await StudentSchema.findById(studentId)
-                // Create new report
-                let month = reportData.Date.getMonth() + 1
-                // Get total Score
-                let totalScore = getTotalScore({homeworkScore, testScore})
-                const newReport = await StudentReportSchema.create({
-                    ...reportData,
-                    Month: month,
-                    Year: reportData.Date.getFullYear(),
-                    Attendance: attendance,
-                    HomeworkScore: homeworkScore,
-                    TestScore: testScore,
-                    TotalScore: totalScore,
-                    HomeworkScoreRequired: homeworkScoreRequired,
-                    TestScoreRequired: testScoreRequired,
-                    ClassID: student.ClassID
-                })
-                return newReport
-            }else{
-                // Update report
-                let updateData = {
-                    HomeworkScore: (homeworkScore === -1) ? reportDb.HomeworkScore : homeworkScore,
-                    HomeworkScoreRequired: (homeworkScore === -1) ? reportDb.HomeworkScoreRequired : homeworkScoreRequired,
-                    TestScore: (testScore === -1) ? reportDb.TestScore : testScore,
-                    TestScoreRequired: (testScore === -1) ? reportDb.TestScoreRequired : testScoreRequired,
-                    Attendance: (!attendance) ? reportDb.Attendance : attendance
-                }
-                
-                let newTotalScore = getTotalScore({homeworkScore: updateData.HomeworkScore, testScore: updateData.TestScore})
-                const updatedReport = await reportDb.updateOne({...updateData, TotalScore: newTotalScore})
-                return updatedReport
-            }
-        }catch(error){
-            console.log(error)
-            return error.message
-        }
-    }
+      const reportDb = await StudentReportSchema.findOne(reportData);
+      if (!reportDb) {
+        let student = await StudentSchema.findById(studentId);
+        // Create new report
+        let month = reportData.Date.getMonth() + 1;
+        // Get total Score
+        let totalScore = getTotalScore({ homeworkScore, testScore });
+        const newReport = await StudentReportSchema.create({
+          ...reportData,
+          Month: month,
+          Year: reportData.Date.getFullYear(),
+          Attendance: attendance,
+          HomeworkScore: homeworkScore,
+          TestScore: testScore,
+          TotalScore: totalScore,
+          HomeworkScoreRequired: homeworkScoreRequired,
+          TestScoreRequired: testScoreRequired,
+          ClassID: student.ClassID,
+        });
+        return newReport;
+      } else {
+        // Update report
+        let updateData = {
+          HomeworkScore:
+            homeworkScore === -1 ? reportDb.HomeworkScore : homeworkScore,
+          HomeworkScoreRequired:
+            homeworkScore === -1
+              ? reportDb.HomeworkScoreRequired
+              : homeworkScoreRequired,
+          TestScore: testScore === -1 ? reportDb.TestScore : testScore,
+          TestScoreRequired:
+            testScore === -1 ? reportDb.TestScoreRequired : testScoreRequired,
+          Attendance: !attendance ? reportDb.Attendance : attendance,
+        };
 
-    // Get Student Report inside Backend
-    static async getStudentReports({classId = null,
-        studentId = null,
-        month = null,
-        year = null,
-        date = null} = {}){
-        try{
-            const reports = await getStudentReports({classId, studentId, month, year, date})
-            return reports.reportsDb
-        }catch(error){
-            console.log(error)
-            return error.message
-        }
+        let newTotalScore = getTotalScore({
+          homeworkScore: updateData.HomeworkScore,
+          testScore: updateData.TestScore,
+        });
+        const updatedReport = await reportDb.updateOne({
+          ...updateData,
+          TotalScore: newTotalScore,
+        });
+        return updatedReport;
+      }
+    } catch (error) {
+      console.log(error);
+      return error.message;
     }
+  }
 
-    static async getStudentMonthlyReportAPI(req, res, next) {
-        try{
-            const {
-              studentid     
-            } = req.params
-            if(!studentid){
-                throw "StudentId is required"
-            }
-            let student = await StudentSchema.findById(studentid)
-            if(!student){
-                throw "Student Not Found"
-            }
-            let reports = await StudentReportSchema.aggregate([
-                { $match: {StudentID: new mongoose.Types.ObjectId(student.id)}},
-                {
-                    $project: {
-                        Month: 1,
-                        Year: 1,
-                        TestScore: 1,
-                        HomeworkScore: 1,
-                        TotalScore: 1,
-                        HomeworkScoreRequired: 1,
-                        TestScoreRequired: 1,
-                        attended: {
-                            $cond: [{$eq: ["$Attendance", true]}, 1, 0]
-                        }
-                    }
-                },
-                {
-                    $group: {
-                        _id: {
-                            Year: "$Year",
-                            Month: "$Month"
-                        },
-                        TotalReport: {$count: {}},
-                        AvgTestScore: {$avg: '$TestScore'},
-                        AvgHomeWorkScore: {$avg: '$HomeworkScore'},
-                        AvgTotalScore: {$avg: '$TotalScore'},
-                        TestScore: {$sum: '$TestScore'},
-                        HomeworkScore: {$sum: '$HomeworkScore'},
-                        TotalScore: {$sum: '$TotalScore'},
-                        HomeworkScoreRequired: {$sum: '$HomeworkScoreRequired'},
-                        TestScoreRequired: {$sum: '$TestScoreRequired'},
-                        Attendance: {$sum: "$attended"}
-                    },
-                    
-                },
-                {
-                    $sort: {_id: 1}
-                }
-                
-            ]).catch(err => {
-                console.log(err)
-            })
-            
-            let reportResponse = {
-                Student: student,
-                Reports: reports,
-                Result: getReportResultTotal(reports)
-            }
-            return res.status(200).json(responseTemplate.successResponse(reportResponse))
-        
-        }catch(error){
-            return res.json(responseTemplate.handlingErrorResponse(error));
-        }
+  // Get Student Report inside Backend
+  static async getStudentReports({
+    classId = null,
+    studentId = null,
+    month = null,
+    year = null,
+    date = null,
+  } = {}) {
+    try {
+      const reports = await getStudentReports({
+        classId,
+        studentId,
+        month,
+        year,
+        date,
+      });
+      return reports.reportsDb;
+    } catch (error) {
+      console.log(error);
+      return error.message;
     }
+  }
 
-    static async getStudentTotalReportAPI(req, res, next) {
-        try{
-            let students = await StudentSchema.find()
-            .catch(err => {
-                throw err
-            })
-            let reportResponses = []
-            await Promise.all(students.map(async (stu) => {
-                let reportResponse = await getStudentReportOverview(stu.StudentID)
-                reportResponses.push(reportResponse)
-            }))
-            return res.status(200).json(responseTemplate.successResponse(reportResponses)); 
-        }catch(error){
-            return res.json(responseTemplate.handlingErrorResponse(error));
-        }
-    }
-       
-    // Get Student Report API
-    static async getStudentDailyReportAPI(req, res, next) {
-        try{
-            const {
-                studentid,
-                month,
-                year,
-                date
-            } = req.query
-            console.log("query",req.query)
-            // student
-            let student = await StudentSchema.findById(studentid)
-            if(!student){
-                throw "Student Not Found"
-            }
-            // report
-            let reportResponse = {
-                Student: student
-            }
-            if(month && year){
-                let reportDb = await getStudentReports({studentId: student.id, month: month, year: year})
-                let totalReport = reportDb.count
-                let reports = reportDb.reportsDb
-                let monthlyResult = getReportResultMonthly(reports, totalReport)
-                reportResponse.Result = monthlyResult
-                reportResponse.Reports = reports
-            }else if(date){
-                let currentDate = new Date(date)
-                let currentMonth = currentDate.getMonth() + 1
-                // date-report
-                let dateReport = await getStudentReports({date: currentDate})
-                if(!dateReport){
-                    throw "Report Not Found"
-                }
-                // month-report
-                let reportDb = await getStudentReports({studentId: student.id, month: currentMonth, year: currentDate.getFullYear()})
-                reportResponse.Reports = reportDb.reportsDb
-                reportResponse.Result = dateReport.reportsDb[0]
-            }else{
-                
-            }
+  static async getStudentMonthlyReportAPI(req, res, next) {
+    try {
+      const { studentid } = req.params;
+      if (!studentid) {
+        throw "StudentId is required";
+      }
+      let student = await StudentSchema.findById(studentid);
+      if (!student) {
+        throw "Student Not Found";
+      }
+      let reports = await StudentReportSchema.aggregate([
+        { $match: { StudentID: new mongoose.Types.ObjectId(student.id) } },
+        {
+          $project: {
+            Month: 1,
+            Year: 1,
+            TestScore: 1,
+            HomeworkScore: 1,
+            TotalScore: 1,
+            HomeworkScoreRequired: 1,
+            TestScoreRequired: 1,
+            attended: {
+              $cond: [{ $eq: ["$Attendance", true] }, 1, 0],
+            },
+          },
+        },
+        {
+          $group: {
+            _id: {
+              Year: "$Year",
+              Month: "$Month",
+            },
+            TotalReport: { $count: {} },
+            AvgTestScore: { $avg: "$TestScore" },
+            AvgHomeWorkScore: { $avg: "$HomeworkScore" },
+            AvgTotalScore: { $avg: "$TotalScore" },
+            TestScore: { $sum: "$TestScore" },
+            HomeworkScore: { $sum: "$HomeworkScore" },
+            TotalScore: { $sum: "$TotalScore" },
+            HomeworkScoreRequired: { $sum: "$HomeworkScoreRequired" },
+            TestScoreRequired: { $sum: "$TestScoreRequired" },
+            Attendance: { $sum: "$attended" },
+          },
+        },
+        {
+          $sort: { _id: 1 },
+        },
+      ]).catch((err) => {
+        console.log(err);
+      });
 
-            return res.status(200).json(responseTemplate.successResponse(reportResponse));
-        
-        }catch(error){
-            return res.json(responseTemplate.handlingErrorResponse(error));
-        }
+      let reportResponse = {
+        Student: student,
+        Reports: reports,
+        Result: getReportResultTotal(reports),
+      };
+      return res
+        .status(200)
+        .json(responseTemplate.successResponse(reportResponse));
+    } catch (error) {
+      return res.json(responseTemplate.handlingErrorResponse(error));
     }
-       
+  }
+
+  static async getStudentTotalReportAPI(req, res, next) {
+    try {
+      let students = await StudentSchema.find().catch((err) => {
+        throw err;
+      });
+      let reportResponses = [];
+      await Promise.all(
+        students.map(async (stu) => {
+          let reportResponse = await getStudentReportOverview(stu.StudentID);
+          reportResponses.push(reportResponse);
+        })
+      );
+      return res
+        .status(200)
+        .json(responseTemplate.successResponse(reportResponses));
+    } catch (error) {
+      return res.json(responseTemplate.handlingErrorResponse(error));
+    }
+  }
+
+  // Get Student Report API
+  static async getStudentDailyReportAPI(req, res, next) {
+    try {
+      const { studentid, month, year, date } = req.query;
+      console.log("query", req.query);
+      // student
+      let student = await StudentSchema.findById(studentid);
+      if (!student) {
+        throw "Student Not Found";
+      }
+      // report
+      let reportResponse = {
+        Student: student,
+      };
+      if (month && year) {
+        let reportDb = await getStudentReports({
+          studentId: student.id,
+          month: month,
+          year: year,
+        });
+        let totalReport = reportDb.count;
+        let reports = reportDb.reportsDb;
+        let monthlyResult = getReportResultMonthly(reports, totalReport);
+        reportResponse.Result = monthlyResult;
+        reportResponse.Reports = reports;
+      } else if (date) {
+        let currentDate = new Date(date);
+        let currentMonth = currentDate.getMonth() + 1;
+        // date-report
+        let dateReport = await getStudentReports({ date: currentDate });
+        if (!dateReport) {
+          throw "Report Not Found";
+        }
+        // month-report
+        let reportDb = await getStudentReports({
+          studentId: student.id,
+          month: currentMonth,
+          year: currentDate.getFullYear(),
+        });
+        reportResponse.Reports = reportDb.reportsDb;
+        reportResponse.Result = dateReport.reportsDb[0];
+      } else {
+      }
+
+      return res
+        .status(200)
+        .json(responseTemplate.successResponse(reportResponse));
+    } catch (error) {
+      return res.json(responseTemplate.handlingErrorResponse(error));
+    }
+  }
+
+  //-------------Get top students-------
+  static async getTopStudent(req, res, next) {
+    try {
+        const students = await StudentReportSchema.find({})
+          .populate("StudentID")
+          .sort({ TotalScore: -1 })
+          .limit(5);
+         return res
+           .status(200)
+           .json(responseTemplate.successResponse(students));
+    } catch (error) {
+      return res.json(responseTemplate.handlingErrorResponse(error));
+    }
+  }
 }
 
 
