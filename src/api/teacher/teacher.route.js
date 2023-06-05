34679781a2420ef94746ad1a7620980c149ed903
your name <you@exemple.com>
2023-06-05 12:40:02 +0700

    api search teacher

--- src/api/teacher/teacher.route.js
+++ src/api/teacher/teacher.route.js
@@ -4,6 +4,7 @@ import TeacherController from "./teacher.controller.js";
 const router = express.Router();
 
 router.route("/").get(TeacherController.getAllTeacher);
+router.route("/find").get(TeacherController.findTeachers);
 router.route("/").post(TeacherController.addTeacher);
 router.get("/:id", TeacherController.getTeacherById);
 router.put("/:id", TeacherController.updateTeacher);
